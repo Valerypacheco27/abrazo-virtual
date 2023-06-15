578913262f4caab370204a392e5503bcbb218de9
Valerypacheco27 <valeryspacheco1@gmail.com>
2023-06-15 18:57:32 -0500

    comit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/* animacion para cambiar el texto cuando la barra se cargue*/
+
+function cambiaMensaje(){
+    document.getElementById("mensaje").innerText = "Espero te guste"
+}
+
+
+setTimeout(cambiaMensaje,2000)
